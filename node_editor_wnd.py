d76ee22f8eebf9d558f83b4ecf64d3406f86a041
koerriva <504097978@qq.com>
2021-01-20 20:43:47 +0800

    implement scene

--- node_editor_wnd.py
+++ node_editor_wnd.py
@@ -1,25 +1,26 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from node_graphics_scene import QDMGraphicsScene
+
 from node_graphics_view import QDMGraphicsView
+from node_scene import Scene
 
 
 class NodeEditorWnd(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.init_ui()
+        self.initUI()
 
-    def init_ui(self):
-        self.setGeometry(800,600,1280,720)
+    def initUI(self):
+        self.setGeometry(800, 600, 1280, 720)
 
         # 创建画布
-        self.grScene = QDMGraphicsScene()
-        self.view = QDMGraphicsView(self.grScene,self)
-
+        self.scene = Scene()
+        self.grScene = self.scene.grScene
+        self.view = QDMGraphicsView(self.grScene, self)
 
         box_layout = QVBoxLayout()
-        box_layout.setContentsMargins(5,5,5,5)
+        box_layout.setContentsMargins(5, 5, 5, 5)
         box_layout.addWidget(self.view)
         self.setLayout(box_layout)
         self.setWindowTitle("Node Editor")
@@ -32,6 +33,5 @@ class NodeEditorWnd(QWidget):
         outlinePen = QPen(Qt.black)
         outlinePen.setWidth(2)
 
-        rect = self.grScene.addEllipse(0,0,100,100,outlinePen,greenBrush)
+        rect = self.grScene.addEllipse(0, 0, 100, 100, outlinePen, greenBrush)
         rect.setFlag(QGraphicsRectItem.ItemIsMovable)
-
--- node_graphics_scene.py
+++ node_graphics_scene.py
@@ -6,8 +6,9 @@ from PyQt5.QtWidgets import *
 
 
 class QDMGraphicsScene(QGraphicsScene):
-    def __init__(self, parent=None):
+    def __init__(self, scene, parent=None):
         super().__init__(parent)
+        self.scene = scene
 
         self._color_background = QColor("#393939")
         self._color_light = QColor("#2f2f2f")
@@ -20,9 +21,11 @@ class QDMGraphicsScene(QGraphicsScene):
         # 设置
         self.gridSize = 20
         self.gridSquares = 5
-        self.scene_width, self.scene_height = 64000, 64000
-        self.setSceneRect(-self.scene_width // 2, -self.scene_height // 2, self.scene_width, self.scene_height)
+
         self.setBackgroundBrush(self._color_background)
+
+    def setGrScene(self, width, height):
+        self.setSceneRect(-width // 2, -height // 2, width, height)
 
     def drawBackground(self, painter: QPainter, rect: QRectF) -> None:
         super().drawBackground(painter, rect)
